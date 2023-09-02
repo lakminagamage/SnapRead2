af198751836e1e44e6444eff4ed243ca629e1d9c
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2023-09-03 01:32:06 +0530

    removed analytics

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC98YU4a1YNgJaTOLBGOv6IB9lbXefDaGU",
   authDomain: "snapread-1e413.firebaseapp.com",
@@ -16,6 +11,4 @@ const firebaseConfig = {
   measurementId: "G-KN9G5W0M1W"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
